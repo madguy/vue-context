a21074cfd754ace1b0cf72fdffbbf11fe3e61f8d
madguy <madguy@drumcan.in>
2021-10-15 09:46:58 +0900

    Update libraries

--- build/webpack-test.mix.js
+++ build/webpack-test.mix.js
@@ -3,5 +3,6 @@ const mix = require('laravel-mix');
 mix
     .setPublicPath('test/js/dist')
     .js('test/js/src/index.js', 'index.js')
+    .vue({ version: 2 })
     .sass('test/js/src/index.scss', 'index.css')
     .sourceMaps();
